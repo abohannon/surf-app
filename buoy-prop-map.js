2f01e09ccdb2bab1cbe9451d97ca01b1530789a2
Adam Bohannon <abo46n2@gmail.com>
2019-06-01 23:54:40 -0700

    Experimenting with approaches to data manipulation

---
+++ buoy-prop-map.js
@@ -0,0 +1,16 @@
+module.exports = {
+  YY: `year`,
+  MM: `month`,
+  DD: `day`,
+  hh: `hour`,
+  mm: `minute`,
+  WDIR: `windDirection`,
+  WSPD: `windSpeed`,
+  WVHT: `waveHeight`,
+  DPD: `dominantWavePeriod`,
+  APD: `averageWavePeriod`,
+  MWD: `meanWaveDirection`,
+  ATMP: `airTemp`,
+  WTMP: `waterTemp`,
+  TIDE: `tide`
+}
